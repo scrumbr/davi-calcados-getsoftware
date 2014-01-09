84955a7f46d26b3cf866ddedde0d6d0444215046
getsoftware2014 <getsoftware2014@gmail.com>
2014-01-09 21:18:23 +0000

    entidade Produto; teste com outro usuário

---
+++ davi-calcados-getsoftware/src/br/com/getsoftware/davicalcados/entity/Produto.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.getsoftware.davicalcados.entity;
+
+/**
+ *
+ * @author Junior Oliveira
+ */
+public class Produto {
+
+    private Long id;
+    private String descricao;
+    private Integer quantidade;
+    private Double valorUnitario;
+    private Double valorVenda;
+
+    //GETTERS AND SETTERS
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Double getValorUnitario() {
+        return valorUnitario;
+    }
+
+    public void setValorUnitario(Double valorUnitario) {
+        this.valorUnitario = valorUnitario;
+    }
+
+    public Double getValorVenda() {
+        return valorVenda;
+    }
+
+    public void setValorVenda(Double valorVenda) {
+        this.valorVenda = valorVenda;
+    }
+}
